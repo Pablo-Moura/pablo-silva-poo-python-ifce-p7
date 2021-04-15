4f47a5fe6c37baec6ff0bbf9fca86604e74b6704
Pablo Silva <pablo.silva.moura62@aluno.ifce.edu.br>
2021-04-15 15:22:00 -0300

    Atividade 4 - Tipos de Triangulo

---
+++ Atividade 4 - 1 Etapa/aula5_4.py
@@ -0,0 +1,21 @@
+"""
+O triângulo pode ser classificado com base no comprimento de
+seus lados em equilátero, isósceles ou escaleno. Todos os três lados de um
+triângulo equilátero têm o mesmo comprimento. Um triângulo isósceles tem dois
+lados que são do mesmo comprimento e um terceiro lado que é diferente comprimento.
+Se todos os lados tiverem comprimentos diferentes, o triângulo é escaleno.
+Escreva um programa que leia os comprimentos dos três lados de um triângulo do usuário. Em seguida, exiba uma mensagem que declara o tipo do triângulo.
+
+"""
+
+from ladosDoTriangulo import *
+
+def descobrirLados():
+    if ladoA == ladoB and ladoA == ladoC:
+        print('O Triângulo tem TODOS os lados IGUAIS, então é um Triângulo Equilátero')
+    elif ladoA == ladoB or ladoA == ladoC or ladoB == ladoC:
+        print('O Triângulo tem DOIS lados IGUAIS, então é um Triângulo Isósceles')
+    else:
+        print('O Triângulo tem TODOS os lados DIFERENTES, então é um Triângulo Escaleno')
+
+descobrirLados()
---
+++ Atividade 4 - 1 Etapa/ladosDoTriangulo.py
@@ -0,0 +1,3 @@
+ladoA = input ("Digite o 1º lado do Triangulo (em cm): ")
+ladoB = input ("Digite o 2º lado do Triangulo (em cm): ")
+ladoC = input ("Digite o 3º lado do Triangulo (em cm): ")
