a4091b1a76bd286c446c627bcce539e87a1350ce
Pablo Silva <pablo.silva.moura62@aluno.ifce.edu.br>
2021-05-21 20:47:34 -0300

    Avaliação 3 - Imprimir Nota Fiscal

---
+++ Atividades[Avaliação] - 1 Etapa/Avaliação Prática - 1 Etapa/folha_pagamento.py
@@ -0,0 +1,37 @@
+from movimento_folha import MovimentoFolha
+
+class FolhaPagamento:
+    def __init__(self, mes, ano, total_descontos, total_proventos):
+        self._mes = mes
+        self._ano = ano
+        self._total_descontos = total_descontos
+        self._total_proventos = total_proventos
+        self._lista_movimentos = []
+        self._colaboradores = []
+
+    def total_salario(self):
+        total_salario = 0
+        for colaborador in self._colaboradores:
+            total_salario += colaborador.get_salario()
+        return total_salario
+
+    def inserir_movimento_fp(self, movimento):
+        if isinstance(movimento, MovimentoFolha):
+            self._lista_movimentos.append(movimento)
+
+    def set_colaborador(self, colaborador):
+        self._colaboradores.append(colaborador)
+
+    def calcular_folha(self):
+        for movimento in self._lista_movimentos:
+            if movimento.get_tipo().value == 1:
+                self._total_proventos += movimento.get_valor()
+            else:
+                self._total_descontos += movimento.get_valor()
+
+        total_pagar = (self.total_salario() + self._total_proventos) - self._total_descontos
+        print('Total de Salários = %.2f\nTotal de Proventos = %.2f\nTotal de Descontos = %.2f\nTotal'
+              ' a Pagar = %.2f\n' % (self.total_salario(), self._total_proventos, self._total_descontos, total_pagar))
+"""        print('Total de Salários = {}\nTotal de Proventos = {}\nTotal de Descontos = {}\n'
+              'Total a Pagar = {}'.format(self.total_salario(), self._total_proventos, self._total_descontos,
+                                          total_pagar))"""
---
+++ Atividades[Avaliação] - 1 Etapa/Avaliação Prática - 1 Etapa/main.py
@@ -0,0 +1,55 @@
+from folha_pagamento import FolhaPagamento
+from colaborador import Colaborador
+from movimento_folha import MovimentoFolha
+from tipomovimento import TipoMovimento
+
+
+def main():
+
+    fp = FolhaPagamento(9, 2019, 0, 0)
+
+    CL01 = Colaborador("100", "Manoel Claudino", "Av 13 de Maio 2081", "88671020",
+                       "Benfica", "60020-060", "124543556-89", 4500.00)
+    CL02 = Colaborador("200", "Carmelina da Silva", "Avenida dos Expedicionários 1200",
+                       "3035-1280", "Aeroporto", "60530-020", "301789435-54",
+                       2500.00)
+    CL03 = Colaborador("300", "Gurmelina Castro Saraiva", "Av João Pessoa 1020",
+                       "3235-1089", "Damas", "60330-090", "350245632-76", 3000.00)
+
+    MF01 = MovimentoFolha(CL01, "Salario", 4500.00, TipoMovimento.P)
+    MF02 = MovimentoFolha(CL01, "Plano Saúde", 1000.00, TipoMovimento.P)
+    MF03 = MovimentoFolha(CL01, "Pensão", 600.00, TipoMovimento.D)
+    MF04 = MovimentoFolha(CL02, "Salario", 2500.00, TipoMovimento.P)
+    MF05 = MovimentoFolha(CL02, "Gratificação", 1000.00, TipoMovimento.P)
+    MF06 = MovimentoFolha(CL02, "Faltas", 600.00, TipoMovimento.D)
+    MF07 = MovimentoFolha(CL03, "Salario", 4500.00, TipoMovimento.P)
+    MF08 = MovimentoFolha(CL03, "Plano Saúde", 1000.00, TipoMovimento.P)
+    MF09 = MovimentoFolha(CL03, "Pensão", 600.00, TipoMovimento.D)
+
+    fp.set_colaborador(CL01)
+    fp.set_colaborador(CL02)
+    fp.set_colaborador(CL03)
+
+    movimentos = [MF01, MF02, MF03, MF04, MF05, MF06, MF07, MF08, MF09]
+
+    for x in movimentos:
+        fp.inserir_movimento_fp(x)
+
+    for m in range(len(movimentos)):
+        if m < 3:
+            CL01.inserir_movimentos_colab(movimentos[m])
+        elif m < 6:
+            CL02.inserir_movimentos_colab(movimentos[m])
+        else:
+            CL03.inserir_movimentos_colab(movimentos[m])
+
+
+    CL01.calcular_salario()
+    CL02.calcular_salario()
+    CL03.calcular_salario()
+
+    fp.calcular_folha()
+
+
+if __name__ == '__main__':
+    main()
